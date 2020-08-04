a1a481f1a6cdb04e4cd9ed25234c7e5cc83ca637
Anastasiia Moskalenko <ubuntu@ip-172-31-5-20.us-east-2.compute.internal>
2020-08-04 10:19:01 +0000

    change dir for entities

---
+++ lib/codebreaker.rb
@@ -0,0 +1,16 @@
+require 'psych'
+require 'i18n'
+require 'fileutils'
+require 'codebreaker/validate.rb'
+require 'locale_config.rb'
+require 'errors/code_length_error.rb'
+require 'errors/code_range_error.rb'
+require 'errors/include_error.rb'
+require 'errors/length_error.rb'
+require 'codebreaker/storage.rb'
+require 'codebreaker/user.rb'
+require 'codebreaker/difficulty.rb'
+require 'codebreaker/version'
+
+module Codebreaker
+end
